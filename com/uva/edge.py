8959d2b827c439dbefb2384cb8a7a4585da75390
Ismail El Helw <ielhelw@cs.vu.nl>
2014-06-04 13:37:53 +0200

    bug fix: Edge needs to be hasable (implement __hash__/__eq__)

--- com/uva/edge.py
+++ com/uva/edge.py
@@ -2,4 +2,8 @@ class Edge(object):
     def __init__(self, first, second):
         self.first = first
         self.second = second
+    def __eq__(self, other):
+        return self.first == other.first and self.second == other.second
+    def __hash__(self):
+        return self.first + self.second
     
