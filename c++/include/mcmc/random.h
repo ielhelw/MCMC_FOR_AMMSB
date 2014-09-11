f2465c35d1a7560082b7a87af661353f78290237
Rutger Hofman <rutger@cs.vu.nl>
2014-09-11 09:07:01 +0200

    -DNDEBUG required to avoid warning

--- c++/include/mcmc/random.h
+++ c++/include/mcmc/random.h
@@ -152,9 +152,11 @@ public:
 		};
 		sample(result, population, count, Inserter());
 
+#ifndef NDEBUG
 		for (auto i : *result) {
 			assert(population.find(i) != population.end());
 		}
+#endif
 
 		return result;
 	}
