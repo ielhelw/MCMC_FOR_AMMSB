92293e44679a842cdea23483b80e80b0cac33309
Rutger Hofman <rutger@cs.vu.nl>
2014-09-18 15:18:46 +0200

    -k option -> -K; catch boost arg exceptions

--- c++/include/mcmc/options.h
+++ c++/include/mcmc/options.h
@@ -196,7 +196,7 @@ public:
 			("eta0", po::value<double>(&eta0)->default_value(1.0), "eta0")
 			("eta1", po::value<double>(&eta1)->default_value(1.0), "eta1")
 
-			("K,k", po::value< ::size_t>(&K)->default_value(300), "K")
+			("K,K", po::value< ::size_t>(&K)->default_value(300), "K")
 			("mini-batch-size,m", po::value< ::size_t>(&mini_batch_size)->default_value(50), "mini_batch_size")
 
 			("epsilon,e", po::value<double>(&epsilon)->default_value(0.05), "epsilon")
