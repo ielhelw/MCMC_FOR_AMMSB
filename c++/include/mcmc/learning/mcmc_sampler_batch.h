441d0472ad1699e1d78b9567a8b0afbacb9416ec
Rutger Hofman <rutger@cs.vu.nl>
2014-09-22 11:37:05 +0200

    Change in verbose report

--- c++/include/mcmc/learning/mcmc_sampler_batch.h
+++ c++/include/mcmc/learning/mcmc_sampler_batch.h
@@ -51,9 +51,9 @@ public:
         // updating \pi and \beta.
 		// theta = Random::random->gamma(eta[0], eta[1], K, 2);        // parameterization for \beta - K by 2
 #if USE_SAMPLE_LATENT_VARS
-		std::cerr << "Use latent vars" << std::endl;
+		std::cerr << "Use sampled latent vars" << std::endl;
 #else
-		std::cerr << "Use closed-form" << std::endl;
+		std::cerr << "Use closed-form latent vars" << std::endl;
 #endif
 
 		std::cerr << "Use fixed values (1.0, 100.0) for eta i.s.o. command-line params" << std::endl;
