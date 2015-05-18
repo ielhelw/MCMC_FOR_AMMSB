421ce0317c59591058566f3fa449ea8a07a1389f
Rutger Hofman <rutger@cs.vu.nl>
2015-05-18 13:55:53 +0200

    Fix RDTSC call in daslib

--- c++/3rdparty/daslib/include/das_time.h
+++ c++/3rdparty/daslib/include/das_time.h
@@ -1,14 +1,18 @@
 #ifndef __DAS_LIB_DAS_TIME_H__
 #define __DAS_LIB_DAS_TIME_H__
 
-#ifdef __GNUC__
+// #ifdef __GNUC__
 
-typedef unsigned long long	das_time_t, *das_time_p;
+#include <inttypes.h>
 
-#define DAS_TIME_RDTSC(res_p)					\
-    __asm __volatile(						\
-		       ".byte 0xf; .byte 0x31"			\
-		      : "=A" (*(res_p)))
+typedef uint64_t	das_time_t, *das_time_p;
+
+#define DAS_TIME_RDTSC(res_p) \
+  do { \
+    uint32_t eax, edx; \
+    __asm __volatile("rdtsc" : "=a" (eax), "=d" (edx)); \
+    *res_p = ((uint64_t)edx << 32) + eax; \
+  } while (0)
 
 static inline void
 das_time_get(das_time_p t)
@@ -22,8 +26,8 @@ void   das_time_d2t(das_time_p t, double d);
 void   das_time_init(int *argc, char **argv);
 void   das_time_end(void);
 
-#else
-#error Need GNU C for asm and long long
-#endif
+// #else
+// #error Need GNU C for asm and long long
+// #endif
 
 #endif
