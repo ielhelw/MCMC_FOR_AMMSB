de9f294b9efc5a65f617ac05252a596ad9d82a85
Rutger Hofman <rutger@cs.vu.nl>
2015-08-27 10:44:27 +0200

    Fix RDMA/RamCloud issue in cmake

--- c++/readme.cmake
+++ c++/readme.cmake
@@ -1,15 +1,23 @@
-module load boost-1.54
+
 THIRDPARTY=`(cd ../../../../3rdparty; pwd)`
 
-mkdir build
-cd build
+# On das4:
+module load boost-1.54
+SPARSEHASH_ROOT=$THIRDPARTY/google-sparsehash
+
+# On das5:
+export BOOST_INCLUDE="$(dirname $(dirname $(which gcc)))/include"
+SPARSEHASH_ROOT=/usr
+
+mkdir build-seq
+cd build-seq
 cmake \
 	-DCMAKE_C_COMPILER=`which gcc` \
 	-DCMAKE_CXX_COMPILER=`which g++` \
 	-DBOOST_ROOT=`dirname $BOOST_INCLUDE` \
 	-DGTEST_ROOT=$THIRDPARTY/gtest-1.7.0 \
 	-DTINYXML2_ROOT=$THIRDPARTY/tinyxml2 \
-	-DSPARSEHASH_ROOT=$THIRDPARTY/google-sparsehash \
+	-DSPARSEHASH_ROOT=$SPARSEHASH_ROOT \
 	-DMCMC_BUILD_MODE=SEQ \
 	..
 cd ..
@@ -22,8 +30,7 @@ cmake \
 	-DBOOST_ROOT=`dirname $BOOST_INCLUDE` \
 	-DGTEST_ROOT=$THIRDPARTY/gtest-1.7.0 \
 	-DTINYXML2_ROOT=$THIRDPARTY/tinyxml2 \
-	-DRAMCLOUD_ROOT=$THIRDPARTY/ramcloud \
-	-DSPARSEHASH_ROOT=$THIRDPARTY/google-sparsehash \
+	-DSPARSEHASH_ROOT=$SPARSEHASH_ROOT \
 	-DMCMC_BUILD_MODE=DISTR \
 	..
 cd ..
